dc46c3b0be60eee2802fd336f318e8ce2a18af3f
noobtree <tree4961@daum.net>
2026-04-29 22:37:50 +0900

    Add Unreal Project Files

---
+++ Source/UE_Shooting/UE_Shooting.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_Shooting.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_Shooting, "UE_Shooting" );
